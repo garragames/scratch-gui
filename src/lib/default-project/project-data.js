4fe3990ef6fe65257dc8af4f6e608acd3a84d776
Jorge Gonzalez Garcia <jorge@MacBook-Pro-de-Jorge.local>
2024-08-21 10:20:00 -0600

    Personalización de la imagen Kori

--- src/lib/default-project/project-data.js
+++ src/lib/default-project/project-data.js
@@ -1,4 +1,4 @@
-import {defineMessages} from 'react-intl';
+import { defineMessages } from 'react-intl';
 import sharedMessages from '../shared-messages';
 import extensions from '../libraries/extensions';
 
@@ -15,7 +15,7 @@ let messages = defineMessages({
     }
 });
 
-messages = {...messages, ...sharedMessages};
+messages = { ...messages, ...sharedMessages };
 
 // use the default message if a translation function is not passed
 const defaultTranslator = msgObj => msgObj.defaultMessage;
@@ -29,90 +29,90 @@ const projectData = translateFunction => {
     const translator = translateFunction || defaultTranslator;
     return ({
         targets: [
-        {
-            "isStage": true,
-            "name": "Stage",
-            "variables": {
-                "`jEk@4|i[#Fk?(8x)AV.-my variable": [
-                    "mi variable",
-                    0
-                ]
+            {
+                "isStage": true,
+                "name": "Stage",
+                "variables": {
+                    "`jEk@4|i[#Fk?(8x)AV.-my variable": [
+                        "mi variable",
+                        0
+                    ]
+                },
+                "lists": {},
+                "broadcasts": {},
+                "blocks": {},
+                "comments": {},
+                "currentCostume": 0,
+                "costumes": [
+                    {
+                        "name": "fondo1",
+                        "dataFormat": "svg",
+                        "assetId": "cd21514d0531fdffb22204e0ec5ed84a",
+                        "md5ext": "cd21514d0531fdffb22204e0ec5ed84a.svg",
+                        "rotationCenterX": 240,
+                        "rotationCenterY": 180
+                    }
+                ],
+                "sounds": [
+                    {
+                        "name": "pop",
+                        "assetId": "83a9787d4cb6f3b7632b4ddfebf74367",
+                        "dataFormat": "wav",
+                        "format": "",
+                        "rate": 44100,
+                        "sampleCount": 1032,
+                        "md5ext": "83a9787d4cb6f3b7632b4ddfebf74367.wav"
+                    }
+                ],
+                "volume": 100,
+                "layerOrder": 0,
+                "tempo": 60,
+                "videoTransparency": 50,
+                "videoState": "on",
+                "textToSpeechLanguage": null
             },
-            "lists": {},
-            "broadcasts": {},
-            "blocks": {},
-            "comments": {},
-            "currentCostume": 0,
-            "costumes": [
-                {
-                    "name": "fondo1",
-                    "dataFormat": "svg",
-                    "assetId": "cd21514d0531fdffb22204e0ec5ed84a",
-                    "md5ext": "cd21514d0531fdffb22204e0ec5ed84a.svg",
-                    "rotationCenterX": 240,
-                    "rotationCenterY": 180
-                }
-            ],
-            "sounds": [
-                {
-                    "name": "pop",
-                    "assetId": "83a9787d4cb6f3b7632b4ddfebf74367",
-                    "dataFormat": "wav",
-                    "format": "",
-                    "rate": 44100,
-                    "sampleCount": 1032,
-                    "md5ext": "83a9787d4cb6f3b7632b4ddfebf74367.wav"
-                }
-            ],
-            "volume": 100,
-            "layerOrder": 0,
-            "tempo": 60,
-            "videoTransparency": 50,
-            "videoState": "on",
-            "textToSpeechLanguage": null
-        },
-        {
-            "isStage": false,
-            "name": "Assistant",
-            "variables": {},
-            "lists": {},
-            "broadcasts": {},
-            "blocks": {
-            },
-            "comments": {},
-            "currentCostume": 0,
-            "costumes": [
-                {
-                    "name": "Assistant",
-                    "bitmapResolution": 1,
-                    "dataFormat": "svg",
-                    "assetId": "735b5e224408fd359b0ddc55f32c6b30",
-                    "md5ext": "735b5e224408fd359b0ddc55f32c6b30.svg",
-                    "rotationCenterX": 42.5,
-                    "rotationCenterY": 42.5
-                }
-            ],
-            "sounds": [
-                {
-                    "name": "pop",
-                    "assetId": "83a9787d4cb6f3b7632b4ddfebf74367",
-                    "dataFormat": "wav",
-                    "format": "",
-                    "rate": 44100,
-                    "sampleCount": 1032,
-                    "md5ext": "83a9787d4cb6f3b7632b4ddfebf74367.wav"
-                }
-            ],
-            "volume": 100,
-            "layerOrder": 1,
-            "visible": true,
-            "x": 0,
-            "y": 0,
-            "size": 100,
-            "direction": 90,
-            "draggable": false,
-            "rotationStyle": "all around"
-        }
+            {
+                "isStage": false,
+                "name": "Assistant",
+                "variables": {},
+                "lists": {},
+                "broadcasts": {},
+                "blocks": {
+                },
+                "comments": {},
+                "currentCostume": 0,
+                "costumes": [
+                    {
+                        "name": "Assistant",
+                        "bitmapResolution": 1,
+                        "dataFormat": "svg",
+                        "assetId": "735b5e224408fd359b0ddc55f32c6b30",
+                        "md5ext": "735b5e224408fd359b0ddc55f32c6b30.svg",
+                        "rotationCenterX": 42.5,
+                        "rotationCenterY": 42.5
+                    }
+                ],
+                "sounds": [
+                    {
+                        "name": "pop",
+                        "assetId": "83a9787d4cb6f3b7632b4ddfebf74367",
+                        "dataFormat": "wav",
+                        "format": "",
+                        "rate": 44100,
+                        "sampleCount": 1032,
+                        "md5ext": "83a9787d4cb6f3b7632b4ddfebf74367.wav"
+                    }
+                ],
+                "volume": 100,
+                "layerOrder": 1,
+                "visible": true,
+                "x": 0,
+                "y": 0,
+                "size": 100,
+                "direction": 90,
+                "draggable": false,
+                "rotationStyle": "all around"
+            }
         ],
         monitors: [],
         extensions: [
--- webpack.config.js
+++ webpack.config.js
@@ -110,25 +110,25 @@ const buildConfig = baseConfig.clone()
     .addPlugin(new HtmlWebpackPlugin({
         chunks: ['gui'],
         template: 'src/playground/index.ejs',
-        title: 'Scratch 3.0 GUI'
+        title: 'Kori 3.0 GUI'
     }))
     .addPlugin(new HtmlWebpackPlugin({
         chunks: ['blocksonly'],
         filename: 'blocks-only.html',
         template: 'src/playground/index.ejs',
-        title: 'Scratch 3.0 GUI: Blocks Only Example'
+        title: 'Kori 3.0 GUI: Blocks Only Example'
     }))
     .addPlugin(new HtmlWebpackPlugin({
         chunks: ['compatibilitytesting'],
         filename: 'compatibility-testing.html',
         template: 'src/playground/index.ejs',
-        title: 'Scratch 3.0 GUI: Compatibility Testing'
+        title: 'Kori 3.0 GUI: Compatibility Testing'
     }))
     .addPlugin(new HtmlWebpackPlugin({
         chunks: ['player'],
         filename: 'player.html',
         template: 'src/playground/index.ejs',
-        title: 'Scratch 3.0 GUI: Player Example'
+        title: 'Kori 3.0 GUI: Player Example'
     }))
     .addPlugin(new CopyWebpackPlugin({
         patterns: [
